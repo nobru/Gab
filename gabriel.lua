aa08a5d2556463b68c5b7e91fdc79e6c480bd2d0
Wesley Victhor Mendes <w.v.mendes.s@gmail.com>
2013-08-12 01:16:05 -0300

    Refactoring. Improve separation of concerns.

--- gabriel.lua
+++ gabriel.lua
@@ -1,31 +1,49 @@
-gabriel = {
-  x = 0, y = 0, w = 32, h = 64, force = 300
+local gabriel = {
+    x = 0,
+    y = 0,
+    w = 32,
+    h = 64,
+    force = 300
 }
-velocity = {x = 0, y = 0}
+
+local velocity = {
+  x = 0,
+  y = 0
+}
+
 force = 0
 
+function gabriel:load()
+    self.body = love.physics.newBody(world, 10, 600 - objects.ground.h - gabriel.h, "dynamic")
+    self.shape = love.physics.newRectangleShape(gabriel.w/2, gabriel.h/2, gabriel.w, gabriel.h)
+    self.fixture = love.physics.newFixture(gabriel.body, gabriel.shape, 1)
+end
+
 function gabriel:draw()
-  love.graphics.setColor(193, 47, 14)
-  love.graphics.rectangle("fill", gabriel.body:getX(), gabriel.body:getY(), gabriel.w, gabriel.h)
-  love.graphics.setColor(0, 0, 0)
-  love.graphics.print("Velocity X: " .. velocity.x, 10, 10)
-  love.graphics.print("Velocity Y: " .. velocity.y, 10, 25)
-  love.graphics.print("Force: " .. force, 10, 50)
+    love.graphics.setColor(193, 47, 14)
+    love.graphics.rectangle("fill", gabriel.body:getX(), gabriel.body:getY(), gabriel.w, gabriel.h)
+    love.graphics.setColor(0, 0, 0)
+    love.graphics.print("Velocity X: " .. velocity.x, 10, 10)
+    love.graphics.print("Velocity Y: " .. velocity.y, 10, 25)
+    love.graphics.print("Force: " .. force, 10, 50)
 end
 
 function gabriel:update()
-  force = gabriel.body:getMass() * velocity.x * 2
-  if love.keyboard.isDown("right") and force <= gabriel.force then
-    gabriel.body:applyForce(gabriel.force, 0)
-    gabriel.body:setFixedRotation(true)
-  elseif love.keyboard.isDown("left") and force >= -gabriel.force then
-    gabriel.body:applyForce(-gabriel.force, 0)
-  end
-  if love.keyboard.isDown("up") and math.floor(velocity.y) == 0 then
-   gabriel.body:applyLinearImpulse(0, -200)
-   gabriel.body:applyForce(velocity.x, 0)
-  end
-  velocity.x, velocity.y = gabriel.body:getLinearVelocity()
+    force = gabriel.body:getMass() * velocity.x * 2
+
+    if love.keyboard.isDown("right") and force <= gabriel.force then
+        gabriel.body:applyForce(gabriel.force, 0)
+        gabriel.body:setFixedRotation(true)
+    elseif love.keyboard.isDown("left") and force >= -gabriel.force then
+        gabriel.body:applyForce(-gabriel.force, 0)
+    end
+
+    if love.keyboard.isDown(" ") and math.floor(velocity.y) == 0 then
+        gabriel.body:applyLinearImpulse(0, -200)
+        gabriel.body:applyForce(velocity.x, 0)
+    end
+
+    velocity.x, velocity.y = gabriel.body:getLinearVelocity()
 end
 
 return gabriel
--- main.lua
+++ main.lua
@@ -1,8 +1,20 @@
 Gamestate = require "hump.gamestate"
 play      = require "scene.playground"
+gabriel = require "gabriel"
 
 function love.load()
   love.graphics.setCaption('Gab The Game')
   Gamestate.registerEvents()
   Gamestate.switch(play)
+  gabriel:load()
+end
+
+function love.draw()
+	gabriel:draw()
+end
+
+function love.update(dt)
+	dt = dt * 2
+	gabriel:update(dt)
+	world:update(dt)
 end
