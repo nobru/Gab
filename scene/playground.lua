1fe4c9f71b40d3466e8d4f6ff38ba578c661d534
Wesley Victhor Mendes <w.v.mendes.s@gmail.com>
2013-08-13 01:46:36 -0300

    Fix ground with new screen size. Remove dead code.

--- scene/playground.lua
+++ scene/playground.lua
@@ -6,15 +6,10 @@ function playground:init()
   love.graphics.setBackgroundColor(210, 222, 227)
 
   objects                 = {}
-  objects.ground          = { w = 800, h = 96 }
-  objects.ground.body     = love.physics.newBody(world, 800/2, 600 - (objects.ground.h/2))
+  objects.ground          = { w = love.graphics.getWidth(), h = 96 }
+  objects.ground.body     = love.physics.newBody(world, love.graphics.getWidth()/2, love.graphics.getHeight() - (objects.ground.h/2))
   objects.ground.shape    = love.physics.newRectangleShape(objects.ground.w, objects.ground.h)
   objects.ground.fixture  = love.physics.newFixture(objects.ground.body, objects.ground.shape)
-
-  objects.box1          = {w = 800, h = 96}
-  objects.box1.body     = love.physics.newBody(world, 800/2, 600 - (objects.ground.h/2))
-  objects.box1.shape    = love.physics.newRectangleShape(objects.ground.w, objects.ground.h)
-  objects.box1.fixture  = love.physics.newFixture(objects.ground.body, objects.ground.shape)
 end
 
 function playground:draw()
